67b2e981b6afb643b5ebffccd3cf860968c11dc2
Mauro Petrini <dev.mpetrini@gmail.com>
2019-12-10 09:08:46 -0300

    change service to scoped

--- Integration.API/Startup.cs
+++ Integration.API/Startup.cs
@@ -34,7 +34,7 @@ namespace Integration.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<IAuthorService, AuthorService>();
+            services.AddScoped<IAuthorService, AuthorService>();
             services.AddDbContext<ApplicationDbContext>(builder =>
             {
                 builder.UseSqlServer(
